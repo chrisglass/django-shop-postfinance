a5858e9633946105ff8e0c8a8fa3fdc2880a23fa
Chris Glass <tribaal@gmail.com>
2011-10-24 15:57:17 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Christopher Glass",
     author_email="tribaal@gmail.com",
     name='django-shop-postfinance',
-    version='0.0.2',
+    version='0.0.3',
     description='An postfinance payment backend',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     url='http://www.django-shop.org/',
